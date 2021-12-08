94a8981a6d9a80d1bf1db62b72c2e542ea8222ee
Greg Roberts <groberts@gmail.com>
2021-12-08 03:38:43 -0500

    Clean up some day 7 mess

--- src/years/year2021/days/day07.rs
+++ src/years/year2021/days/day07.rs
@@ -10,7 +10,7 @@ fn a() -> i64 {
 
     let mut min = i64::MAX;
     for i in 0..max+1 {
-        let diff = positions.iter().map(|x| (x - i).abs()*((x - i).abs() + 1)/2).reduce(|a, b| a+b).unwrap();
+        let diff = positions.iter().map(|x| (x - i).abs()).reduce(|a, b| a+b).unwrap();
         if diff < min {
             min = diff;
         }
@@ -20,5 +20,16 @@ fn a() -> i64 {
 }
 
 fn b() -> i64 {
-    0
+    let positions: Vec<i64> = fs::read_to_string("input/2021/day07.txt").unwrap().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
+    let max = positions.iter().max().unwrap();
+
+    let mut min = i64::MAX;
+    for i in 0..max+1 {
+        let diff = positions.iter().map(|x| (x - i).abs()*((x - i).abs() + 1)/2).reduce(|a, b| a+b).unwrap();
+        if diff < min {
+            min = diff;
+        }
+    }
+
+    min
 }
